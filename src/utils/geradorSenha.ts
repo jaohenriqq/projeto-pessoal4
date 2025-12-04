d726c4bfd1cffb4632b537a44ed522b133609cb5
João Henrique <joaohen.t8@gmail.com>
2025-12-04 07:06:37 -0300

    Atualizar o geradorSenha.ts

--- src/utils/geradorSenha.ts
+++ src/utils/geradorSenha.ts
@@ -1,4 +1,4 @@
-// src/utils/geradorSenha.ts
+
 export function gerarSenha(length = 8, incluirNumeros = true, incluirSimbolos = false): string {
     let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
     if (incluirNumeros) chars += '0123456789';
